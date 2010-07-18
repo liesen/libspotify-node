39581b1c928b16cc77ea8d57f6ac69e7b72ed7a8
liesen <johanliesen@gmail.com>
2010-07-18 21:28:22 +0200

    Cleaned session header file: removed unused methods + fixed indentation

--- src/session.h
+++ src/session.h
@@ -11,16 +11,14 @@ class Session : public node::EventEmitter {
   static void Initialize(v8::Handle<v8::Object> target);
 
   static v8::Handle<v8::Value> New(const v8::Arguments& args);
-  static v8::Handle<v8::Value> Login(const v8::Arguments& args);
 
-  void Login(const char* username, const char* password);
+  static v8::Handle<v8::Value> Login(const v8::Arguments& args);
 
   static v8::Handle<v8::Value> Logout(const v8::Arguments& args);
 
-  void Logout();
-
-  static v8::Handle<v8::Value> ConnectionStateGetter(v8::Local<v8::String> property,
-                                               const v8::AccessorInfo& info);
+  static v8::Handle<v8::Value> ConnectionStateGetter(
+      v8::Local<v8::String> property,
+      const v8::AccessorInfo& info);
 
   /*
   static v8::Handle<v8::Value> PlaylistContainer(v8::Local<v8::String> property,
@@ -29,7 +27,7 @@ class Session : public node::EventEmitter {
 
   // Gets the user associated with a session
   static v8::Handle<v8::Value> UserGetter(v8::Local<v8::String> property,
-                                    const v8::AccessorInfo& info);
+                                          const v8::AccessorInfo& info);
 
   void EmitLogMessage(const char* message);
 
