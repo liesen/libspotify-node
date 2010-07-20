bf2223d467786c29729f6b100b3d6b582228fac4
Rasmus Andersson <rasmus@notion.se>
2010-07-20 16:33:08 +0200

    copyright for atomic_queue adjusted (Notion is the alias of Rasmus Andersson, but the latter is the canonical form)

--- src/atomic_queue.h
+++ src/atomic_queue.h
@@ -23,7 +23,7 @@
 
 
 
-  Copyright (c) 2009 Notion <http://notion.se/>
+  Copyright (c) 2009-2010 Rasmus Andersson <http://hunch.se/>
 
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
