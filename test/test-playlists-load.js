9e92032e3fa0e7eb9a2afe6cd25f7691df34ed6a
Rasmus Andersson <rasmus@notion.se>
2010-07-21 02:10:51 +0200

    do not dump massive output in the test

--- test/test-playlists-load.js
+++ test/test-playlists-load.js
@@ -2,7 +2,10 @@ require('./test');
 
 createSession(function (session) {
   session.playlists.addListener('load', function () {
-    sys.print(sys.inspect(this));
+    //sys.print(sys.inspect(this));
+    // this will fail if you don't have any playlists...
+    assert.strictEqual(typeof this.length, "number");
+    assert.ok(this.length > 0);
     session.logout(assert.ifError);
   })
 });
